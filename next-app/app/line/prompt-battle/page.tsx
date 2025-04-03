0fbcbdb83f89d84be45a1ce0623d5f96b64f93ba
ryu_wsl <ry-tanaka@sios.com>
2025-04-03 08:03:49 +0000

    feature

--- next-app/app/line/prompt-battle/page.tsx
+++ next-app/app/line/prompt-battle/page.tsx
@@ -1,86 +1,83 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import { axiosClient } from "@/service/axiosClient";
-import { EnemyType } from "@/types/PromptType";
+import { LoadingMainComponent } from "@/components/LoadingMainComponent";
+import { PromptFormType } from "@/types/PromptType";
+
+import { CombatResult } from "./components/DuelResult";
+import { PromptForm } from "./components/PromptForm";
+import { usePromptBattle } from "./hooks/usePromptBattle";
 
 
 
 
 export default function PromptBattle() {
-  const [data, setData] = useState<EnemyType>({
-    enemyID: "",
-    name: "",
-    imageUrl: ""
-  });
-  const onClick = async () => {
-    const res = await axiosClient.get<EnemyType>("/api/line/battle-test");
-    const tmp = res.data;
-    setData(tmp);
-  };
+  // const [data, setData] = useState<EnemyType>({
+  //   enemyID: "",
+  //   name: "",
+  //   imageUrl: ""
+  // });
+  // const onClick = async () => {
+  //   const res = await axiosClient.get<EnemyType>("/api/line/battle-test");
+  //   const tmp = res.data;
+  //   setData(tmp);
+  // };
 
-  const onClickNext = async () => {
-    const res = await axiosClient.get<EnemyType>("/api/line/battle");
-    const tmp = res.data;
-    setData(tmp);
-  }
-  // const {
-  //   enemyData,
-  //   isLoadingEnemy,
-  //   mutateEnemy,
-  //   isValidating,
-  //   combatResult,
-  //   isMutatingCombat,
-  //   trigger,
-  // } = usePromptBattle();
+  // const onClickNext = async () => {
+  //   const res = await axiosClient.get<EnemyType>("/api/line/battle");
+  //   const tmp = res.data;
+  //   setData(tmp);
+  // }
+  const {
+    enemyData,
+    isLoadingEnemy,
+    mutateEnemy,
+    isValidating,
+    combatResult,
+    isMutatingCombat,
+    trigger,
+  } = usePromptBattle();
 
   // const { userData, isLoadingUser, mutateUser } = useUser();
 
-  // const [isDuel, setDuel] = useState(false);
+  const [isDuel, setDuel] = useState(false);
 
-  // const submitPrompt = async (formData: PromptFormType) => {
-  //   if (!enemyData) {
-  //     await mutateEnemy();
-  //     return;
-  //   }
-  //   console.log("submit", formData);
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth",
-  //   });
-  //   await trigger({ ...formData, enemyID: enemyData.enemyID });
-  //   setDuel((prev) => !prev);
-  // };
+  const submitPrompt = async (formData: PromptFormType) => {
+    if (!enemyData) {
+      await mutateEnemy();
+      return;
+    }
+    console.log("submit", formData);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    await trigger({ ...formData, enemyID: enemyData.enemyID });
+    setDuel((prev) => !prev);
+  };
 
-  // const onClickSubmitNextBattle = async () => {
-  //   await mutateEnemy();
-  //   await mutateUser();
-  //   setDuel(false);
-  // };
-  // useEffect(() => {
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth",
-  //   });
-  // }, [isDuel]);
+  const onClickSubmitNextBattle = async () => {
+    await mutateEnemy();
+    // await mutateUser();
+    setDuel(false);
+  };
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [isDuel]);
 
-  // if (isMutatingCombat) return <LoadingMainComponent />;
-  // if (isLoadingEnemy || !enemyData) return <LoadingMainComponent />;
+  if (isMutatingCombat) return <LoadingMainComponent />;
+  if (isLoadingEnemy || !enemyData) return <LoadingMainComponent />;
   // if (isLoadingUser || !userData) return <LoadingMainComponent />;
 
   return (
     <>
       <main className=" flex w-full flex-col items-center gap-10 pb-10">
-        <button onClick={onClick}
-          className="flex w-full items-center justify-center rounded bg-slate-200 py-4 text-2xl font-bold">
-          deta 取得
-        </button>
-        <button onClick={onClickNext} className="flex w-full items-center justify-center rounded bg-slate-200 py-4 text-2xl font-bold">
-          next 取得
-        </button>
-        <pre>{JSON.stringify(data)}</pre>
-        {/* <div className="flex w-full flex-col items-center gap-3 px-2 py-6">
+
+        <div className="flex w-full flex-col items-center gap-3 px-2 py-6">
           <span className="text-2xl font-bold">
             対戦チャンピオン
             <span />
@@ -112,15 +109,6 @@ export default function PromptBattle() {
           )}
         </div>
 
-        <div className="flex w-full flex-col items-center gap-3 p-6">
-          <div className="flex w-full flex-row justify-between text-5xl font-bold">
-            <span className="text-red-500">{userData.winCount}勝</span>
-            <span className="text-blue-500">{userData.lossCount}敗</span>
-          </div>
-          <div className="flex w-full flex-row justify-center text-2xl font-bold">
-            <span>{userData.hotStreak}連勝中！</span>
-          </div>
-        </div> */}
       </main>
     </>
   );
