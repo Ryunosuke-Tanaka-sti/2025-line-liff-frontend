7afd6e428a9cd4e6287bafd871e800c524f61baa
ryu_wsl <ry-tanaka@sios.com>
2025-04-05 06:31:17 +0000

    feature:Next GAS OAuth

--- next-app/app/google/page.tsx
+++ next-app/app/google/page.tsx
@@ -2,24 +2,57 @@
 
 import { useEffect, useState } from "react"
 
-const getClientCookies = () => {
-    return document.cookie.split(';').reduce((acc, cookie) => {
-        const [name, value] = cookie.trim().split('=')
-        return { ...acc, [name]: value }
-    }, {})
-}
+// const getClientCookies = () => {
+//     return document.cookie.split(';').reduce((acc, cookie) => {
+//         const [name, value] = cookie.trim().split('=')
+//         return { ...acc, [name]: value }
+//     }, {})
+// }
 export default function GooglePage() {
-    const [cookies, setCookies] = useState<Record<string, string>>({})
-
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
-        setCookies(getClientCookies())
-    }, [])
+        if (isLoading) {
+            const verify = async () => {
+                const res = await fetch('/api/google-auth/verify')
+                if (res.status === 200) {
+                    setIsLoading(false)
+                } else {
+                    alert('認可に失敗しました')
+                }
+            }
+            verify()
+        }
+    }, [isLoading])
 
+    // const [cookies, setCookies] = useState<Record<string, string>>({})
+
+    // useEffect(() => {
+    //     setCookies(getClientCookies())
+    // }, [])
+    const onClick = async () => {
+        const res = await fetch('/api/google-auth/test')
+        console.log(res)
+    }
+
+    if (isLoading) {
+        return (
+            <div className="flex h-screen items-center justify-center">
+                <h1 className="text-2xl font-bold">Loading...</h1>
+            </div>
+        )
+    }
     return (
         <>
             <main className="flex w-full flex-col gap-2">
-
-                <div className="mt-8">
+                <div className="flex max-w-xl flex-col gap-2 p-4">
+                    <a
+                        onClick={onClick}
+                        className="flex items-center justify-center rounded-lg bg-white px-8 py-2 shadow transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-md"
+                    >
+                        TEST Req
+                    </a>
+                </div>
+                {/* <div className="mt-8">
                     <h2 className="mb-4 text-xl font-bold">Client-side Cookies</h2>
                     {Object.entries(cookies).map(([name, value]) => (
                         <div key={name} className="mb-2 rounded-lg border p-4">
@@ -27,15 +60,15 @@ export default function GooglePage() {
                             <p className="whitespace-normal break-words text-gray-600">{value}</p>
                         </div>
                     ))}
-                </div>
-                <div className="flex max-w-xl flex-col gap-2 p-4">
+                </div> */}
+                {/* <div className="flex max-w-xl flex-col gap-2 p-4">
                     <a
-                        href="/api/google-auth"
+                        href="/api/google-auth/verify"
                         className="flex items-center justify-center rounded-lg bg-white px-8 py-2 shadow transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-md"
                     >
                         認可へ飛ぶ
                     </a>
-                </div>
+                </div> */}
             </main>
         </>
     )
